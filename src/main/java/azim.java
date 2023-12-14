ca2a5c0b1c10d64a87a85b1654114df9dcac5ef2
AzimKaya <azimkaya777@gmail.com>
2023-12-14 18:45:10 +0300

    first commit

---
+++ src/main/java/azim.java
@@ -0,0 +1,9 @@
+public class azim {
+    public static void main(String[] args) {
+
+        System.out.println("Azim kaya'Nın calısma alani");
+
+
+
+    }
+}
---
+++ src/main/java/ortakClass.java
@@ -0,0 +1,21 @@
+public class ortakClass {
+    public static void main(String[] args) {
+        System.out.println("Githup calısmalrı dedvam ediyor");
+
+        /*
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AzimKaya/GithubTeamWork-t127.git
+git push -u origin main
+
+         */
+
+
+
+
+    }
+}
